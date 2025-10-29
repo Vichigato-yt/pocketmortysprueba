398b67de436442c928e0ec3ec766f792754cd3d3
vicejo1009 <vicejo1009@gmail.com>
2025-10-29 19:02:40 +0000

    Instalacion de API de Rick Y Morty  - SE ordenaron los Mortys existentes en la API actual (se va a instalar una API de mortys más completa)

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MortyListScreen from '@/app/screens/MortyListScreen';
+import CharacterDetailScreen from '@/app/screens/CharacterDetailScreen';
+
+export type RootStackParamList = {
+  MortyList: undefined;
+  CharacterDetail: { id: number };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function index() {
+  return (
+    <SafeAreaProvider>
+        <Stack.Navigator initialRouteName="MortyList">
+          <Stack.Screen
+            name="MortyList"
+            component={MortyListScreen}
+            options={{ title: 'Morty Selector Index' }}
+          />
+          <Stack.Screen
+            name="CharacterDetail"
+            component={CharacterDetailScreen}
+            options={{ title: 'Morty Detail' }}
+          />
+        </Stack.Navigator>
+    </SafeAreaProvider>
+  );
+}
